562b63bb3712fc5b3ff693e05728614410288126
neda <neda.ghodrati@gmail.com>
2021-12-03 11:48:42 -0800

    Audio and Phonetic set

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
         Listen to it
       </a>
       <br />
