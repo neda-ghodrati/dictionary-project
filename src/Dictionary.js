b6a2b48c95aef266968e52abda684ce234a8bf2a
neda <neda.ghodrati@gmail.com>
2021-12-06 11:30:36 -0800

    pictures Added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyWord);
   let [results, setResults] = useState();
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  let [photos, setPhotos] = useState();
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    let pexelsApiKey =
+      "563492ad6f91700001000001d412090252ad4db6ac8b9728833e1538";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -45,6 +59,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
